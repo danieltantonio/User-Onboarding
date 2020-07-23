04607329e5fd2509754425499b6a14abefdb23c4
Daniel Antonio <danieltomantonio@gamil.com>
2020-07-23 12:54:12 -0700

    Added Cypress package module and created/finished test file

---
+++ user-onboarding/cypress/integration/form_test.js
@@ -0,0 +1,53 @@
+describe('Inputs and buttons', () => {
+  it('can navigate to the site', () => {
+      cy.visit('http://localhost:3000')
+      cy.url().should('include', 'localhost')
+  })
+
+  it('can select and type within the name field', () => {
+      cy.get('input[name="name"]')
+        .type('Bruh')
+        .should('have.value', 'Bruh')
+  })
+
+  it('can select and type within the email field', () => {
+      cy.get('input[name="email"]')
+        .type('bruh@bruh.com')
+        .should('have.value', 'bruh@bruh.com')
+  })
+
+  it('can select and type within the password field', () => {
+      cy.get('input[name="password"]')
+        .type('N0t@str0ngP@$$')
+        .should('have.value', 'N0t@str0ngP@$$')
+  })
+
+  it('can select the ToS input', () => {
+    cy.get(':nth-child(6) > input')
+      .click()
+  })
+
+  it('can see if the user can submit form data', () => {
+      cy.get('button')
+        .should('not.be.disabled')
+  })
+
+  it('can check to see if any of the form fields are blank', () => {
+      cy.get('input[name="name"]')
+        .should('not.have.value', "")
+
+      cy.get('input[name="email"]')
+        .should('not.have.value', "")
+    
+      cy.get('input[name="password"]')
+        .should('not.have.value', "")
+
+      cy.get(':nth-child(6) > input')
+        .should('not.have.value', '')
+  })
+
+  it('can submit form data', () => {
+      cy.get('button')
+        .click()
+  })
+})
